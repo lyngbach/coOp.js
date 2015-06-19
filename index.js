e48d411a35a70e78f111ae12927b986230538845
Rune Lyngbach Jensen <lyngbachjensen@gmail.com>
2015-06-19 14:27:32 +0200

    collaborative

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+	app = express(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http);
+
+// app routing
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+	app.use("/css", express.static(__dirname + "/css"));
+	app.use("/node_modules", express.static(__dirname + "/node_modules"));
+});
+
+io.sockets.on('connection', function (socket) {
+	console.log('a user session', socket.id);
+
+	socket.on('sendText', function (paragraphs) {
+		console.log('server recived text:', paragraphs);
+
+		console.log('sending to all other users');
+
+		socket.broadcast.emit('msgBroadcast', paragraphs);
+	});
+});
+
+http.listen(2000, function () {
+	console.log('listening on *:2000');
+});
